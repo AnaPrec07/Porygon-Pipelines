4c2a7e63bc3b414ea163c31b206eb6d51a91c4c9
Ana Preciado <49446784+AnaPrec07@users.noreply.github.com>
2025-10-08 11:07:53 -0500

    POPIP-42: Init Abstract Pipeline Class (#2)
    * POPIP-42: Init Abstract Pipeline Class.
    
    * POPIP-42: Improve module docstring.
    
    * POPIP-42: Remove table_name parameter from pipeline.
    
    ---------
    
    Co-authored-by: Ana  Preciado <anapreciado@Anas-MacBook-Pro.local>

---
+++ src/core/pipeline.py
@@ -0,0 +1,51 @@
+"""
+core/pipeline.py
+
+This module defines the Pipeline abstract class, which serves as a template for all pipelines in this repository.
+It provides a consistent interface for data extraction, transformation, and loading.
+"""
+
+from src.core.bigquery_client import BigQueryClient
+from abc import ABC, abstractmethod
+
+class Pipeline(ABC):
+    """
+    Abstract base class for all data pipelines.
+    """
+    def __init__(self):
+        """
+        Initialize abstract pipeline class.
+        """
+        self.bq_client = BigQueryClient()
+
+    @abstractmethod
+    def extract(self):
+        """
+        Abstract method to extract data from a specified table.
+        Must be implemented by subclasses.
+        """
+        pass
+
+    @abstractmethod
+    def transform(self):
+        """
+        Abstract method to transform extracted data.
+        Must be implemented by subclasses.
+        """
+        pass
+
+    @abstractmethod
+    def load(self):
+        """
+        Abstract method to load transformed data.
+        Must be implemented by subclasses.
+        """
+        pass
+
+    def run(self):
+        """
+        Run the pipeline.
+        """
+        self.extract()
+        self.transform()
+        self.load()
